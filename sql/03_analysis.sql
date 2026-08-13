db7a89a75d6934198e3cec4c21209161847bff3e
Raymon Watson <raymonswatson@gmail.com>
2026-08-13 14:24:11 +1000

    Update 03_analysis.sql

--- sql/03_analysis.sql
+++ sql/03_analysis.sql
@@ -1,1 +1,12 @@
 
+--What is the total revenue generated?
+--Which product category generates the most revenue?
+--What is the average order quantity?
+--Which cities generate the most revenue?
+
+
+--Who are the top 10 highest spending customers?
+--Which month has the highest sales?
+--What is the average spending per customer?
+--Which product category sells the most units?
+--What is the most common payment method?
